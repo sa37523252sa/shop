37e3c6c13085f48ba527cc4298270e6d591e5e51
sa37523252sa <ghh8dd6rgt@privaterelay.appleid.com>
2025-12-11 17:12:04 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("./authController");
 
+// 從 authController 解構出函式
+const { register, login } = authController;
 
 router.post("/register", register);
 router.post("/login", login);
